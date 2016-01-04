d8d3c515cbe3bfa307033bd27ca05d34f59d287a
vv1133 <zhengdi_vv1133@foxmail.com>
2016-01-04 22:47:37 +0800

    add tcp send close

---
+++ tcp_send_close/client.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+nc localhost 5188 | wc -c
---
+++ tcp_send_close/server.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<unistd.h>
+
+#define ERR_EXIT(m) \
+	do { \
+		perror(m); \
+		exit(EXIT_FAILURE); \
+	} while (0)
+
+int main(void)
+{
+	int listenfd;
+	if ((listenfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		ERR_EXIT("socket error");
+
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(5188);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	int on = 1;
+	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
+		ERR_EXIT("setsockopt error");
+
+	if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+		ERR_EXIT("bind error");
+
+	if (listen(listenfd, SOMAXCONN) < 0)
+		ERR_EXIT("listen error");
+
+	int len = 10*1024*1024;
+	char *buf = malloc(len);
+	if (buf == NULL)
+		ERR_EXIT("malloc error");
+	while (1) {
+		int conn;
+		struct sockaddr_in peeraddr;
+		socklen_t peerlen = sizeof(peeraddr);
+		if ((conn = accept(listenfd, (struct sockaddr *)&peeraddr, &peerlen)) < 0)
+			ERR_EXIT("accept error");
+		printf("recv connect ip=%s port=%d\n", inet_ntoa(peeraddr.sin_addr), ntohs(peeraddr.sin_port));
+
+		sleep(5);
+		int writelen;
+		writelen = send(conn, buf, len, 0);
+		printf("writelen = %d\n", writelen);
+		close(conn);
+		printf("close\n");
+	}
+	close(listenfd);
+
+	return 0;
+}
